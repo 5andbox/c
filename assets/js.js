1767f69b8d4eee0bed1688c8b39b5106e489de84
5andbox <etabtilox@gmail.com>
2025-07-24 01:56:54 +0100

    Add files via upload

--- assets/js.js
+++ assets/js.js
@@ -19,19 +19,19 @@ const monthYear = document.getElementById('month-year');
 
   const eventsMap = {
     '2025-07': {
-       3: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" },
-      10: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" },
+       3: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
+      10: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
       11: { text: "POKER TOURNAMENT", url: "https://5andbox.github.io/c/poker/index.html" },
-      17: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" },
-      24: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/hnightquiz/index.html" },
-      31: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" },
+      17: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
+      24: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
+      31: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
       24: { text: "HOUSE OF STAKE", url: "https://5andbox.github.io/c/houseofstake/index.html" }
     },
     '2025-08': {
-       7: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" },
-      14: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" },
-      21: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" },
-      28: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/nightquiz/index.html" }
+       7: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
+      14: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
+      21: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" },
+      28: { text: "NIGHT QUIZ", url: "https://5andbox.github.io/c/quiznight/index.html" }
     }
   };
 
